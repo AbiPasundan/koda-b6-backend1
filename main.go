dc601cc1c1246b52fb20d6609ab8f47a014e6a1e
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-02-27 05:36:07 +0700

    fix: fix bug while login

--- main.go
+++ main.go
@@ -8,15 +8,12 @@ import (
 
 func main() {
 	r := gin.Default()
-	// pkg.OriginAuth()
 	authorized := r.Group("/")
 	authorized.Use(pkg.CookieTool())
 	{
 		authorized.GET("/", pkg.Home)
-		authorized.GET("/user/:id", pkg.UserSearch)
+		authorized.GET("/users/:id", pkg.UserSearch)
 	}
-	// r.GET("/", pkg.CookieTool(), pkg.Home)
-	// r.GET("/users/:id", pkg.CookieTool(), pkg.UserSearch)
 	r.GET("/register", pkg.Register)
 	r.POST("/register", pkg.RegisterPost)
 	r.GET("/login", pkg.Login)
--- pkg/auth.go
+++ pkg/auth.go
@@ -14,15 +14,12 @@ var data = Users{}
 
 func CookieTool() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Get cookie
 		if cookie, err := c.Cookie("label"); err == nil {
 			if cookie == "ok" {
 				c.Next()
 				return
 			}
 		}
-
-		// Cookie verification failed
 		c.JSON(http.StatusForbidden, gin.H{"error": "Forbidden with no cookie"})
 		c.Abort()
 	}
@@ -41,31 +38,31 @@ func Home(ctx *gin.Context) {
 func UserSearch(ctx *gin.Context) {
 	id := ctx.Param("id")
 	i, err := strconv.Atoi(id)
-	if i == 1 {
-		i = 1
-	}
 
 	if err != nil {
 		ctx.JSON(400, Response{
-			Success: true,
+			Success: false,
 			Message: "Bad Request",
-			Results: 0,
+			Results: nil,
 		})
+		return
 	}
+
 	for _, user := range ListUser {
 		fmt.Println(user.Id)
 		if int(user.Id) == i {
 			ctx.JSON(200, Response{
 				Success: true,
 				Message: "berhasil",
-				Results: ListUser[i],
+				Results: user,
 			})
 			return
 		}
 	}
+
 	ctx.JSON(404, Response{
-		Success: true,
-		Message: "eubofceobu",
+		Success: false,
+		Message: "User not found",
 		Results: nil,
 	})
 }
@@ -158,6 +155,7 @@ func LoginPost(ctx *gin.Context) {
 			ctx.SetCookie("label", "ok", 30, "/", "localhost", false, true)
 
 			ctx.String(200, "Login success!")
+			return
 		}
 	}
 	ctx.ShouldBindJSON(&data)
@@ -167,155 +165,3 @@ func LoginPost(ctx *gin.Context) {
 		Results: nil,
 	})
 }
-
-// func OriginAuth() {
-// 	r := gin.Default()
-// 	r.GET("/", CookieTool(), func(ctx *gin.Context) {
-// 		ctx.JSON(http.StatusOK, Response{
-// 			Success: true,
-// 			Message: "Data User",
-// 			Results: ListUser,
-// 		})
-// 	})
-
-// 	r.GET("/users/:id", CookieTool(), func(ctx *gin.Context) {
-// 		id := ctx.Param("id")
-// 		i, err := strconv.Atoi(id)
-// 		if i == 1 {
-// 			i = 1
-// 		}
-
-// 		if err != nil {
-// 			ctx.JSON(400, Response{
-// 				Success: true,
-// 				Message: "Bad Request",
-// 				Results: 0,
-// 			})
-// 		}
-// 		fmt.Println(i)
-
-// 		for _, user := range ListUser {
-// 			fmt.Println(user.Id)
-// 			if int(user.Id) == i {
-// 				ctx.JSON(200, Response{
-// 					Success: true,
-// 					Message: "berhasil",
-// 					Results: ListUser[i-1],
-// 				})
-// 				return
-// 			}
-// 		}
-// 		ctx.JSON(404, Response{
-// 			Success: true,
-// 			Message: "eubofceobu",
-// 			Results: nil,
-// 		})
-
-// 	})
-
-// 	r.GET("/register", func(ctx *gin.Context) {
-// 		ctx.JSON(http.StatusOK, Response{
-// 			Success: true,
-// 			Message: "Halaman Register Silahkan isi Di dengan POST",
-// 		})
-// 	})
-
-// 	r.POST("/register", func(ctx *gin.Context) {
-// 		var err = ctx.ShouldBindJSON(&data)
-// 		if err != nil {
-// 			ctx.JSON(http.StatusOK, Response{
-// 				Success: true,
-// 				Message: "Something Gone Wrong",
-// 			})
-// 		} else {
-// 			for x := range ListUser {
-// 				wordToCheck := "@"
-
-// 				if !strings.Contains(data.Email, wordToCheck) {
-// 					ctx.JSON(400, Response{
-// 						Success: false,
-// 						Message: "That is not an email",
-// 						Results: ListUser,
-// 					})
-// 					return
-// 				} else {
-// 					if data.Email == ListUser[x].Email {
-// 						ctx.JSON(400, Response{
-// 							Success: false,
-// 							Message: "Duplicated Email Not palid",
-// 							Results: ListUser,
-// 						})
-// 						return
-// 					}
-// 					if len(data.Password) <= 8 {
-// 						ctx.JSON(400, Response{
-// 							Success: true,
-// 							Message: "Password terlalu lemah",
-// 							Results: ListUser,
-// 						})
-// 						return
-// 					}
-// 				}
-// 			}
-
-// 			argon := argon2.DefaultConfig()
-// 			encoded, err := argon.HashEncoded([]byte(data.Password))
-// 			if err != nil {
-// 				panic(err)
-// 			}
-
-// 			ListUser = append(ListUser, Users{
-// 				Id:       idCounter(),
-// 				Email:    data.Email,
-// 				Name:     data.Name,
-// 				Password: string(encoded),
-// 			})
-
-// 			ctx.JSON(200, Response{
-// 				Success: true,
-// 				Message: "Back End is Running Well test",
-// 				Results: ListUser,
-// 			})
-// 		}
-// 	})
-
-// 	r.GET("/login", func(ctx *gin.Context) {
-// 		ctx.JSON(http.StatusOK, Response{
-// 			Success: true,
-// 			Message: "Halaman Login Silahkan isi Di dengan POST",
-// 		})
-// 	})
-
-// 	r.POST("/login", func(ctx *gin.Context) {
-// 		for _, user := range ListUser {
-// 			ok, err := argon2.VerifyEncoded(
-// 				[]byte(data.Password),
-// 				[]byte(user.Password),
-// 			)
-
-// 			if err != nil {
-// 				panic(err)
-// 			}
-
-// 			if data.Email == user.Email && ok {
-// 				// Loggined()
-// 				// fmt.Println(IsAuthenticated)
-// 				// // ctx.Redirect(http.StatusFound, "/")
-// 				// ctx.JSON(http.StatusOK, Response{
-// 				// 	Success: true,
-// 				// 	Message: "Ok email dan password sama",
-// 				// })
-// 				ctx.SetCookie("label", "ok", 30, "/", "localhost", false, true)
-// 				ctx.String(200, "Login success!")
-// 			} else {
-// 				ctx.JSON(400, Response{
-// 					Success: false,
-// 					Message: "Email or Password Wrong",
-// 					Results: nil,
-// 				})
-// 			}
-// 		}
-// 	})
-
-// 	r.Run("localhost:8888")
-// }
